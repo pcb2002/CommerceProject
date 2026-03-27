e5b777ecd9fa977b045bbc2676b7a86196af25c7
pcb2002 <machat350@gmail.com>
2026-03-27 15:25:11 +0900

    [Modify]: apply feedback and add exaption

--- src/step3/Category.java
+++ src/step3/Category.java
@@ -1,9 +1,7 @@
 package step3;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Category {
     private String categoryName;
@@ -14,37 +12,22 @@ public class Category {
         this.products = products;
     }
 
-    public static Category createElectronicsCategory() {
-        List<Product> electronicList = List.of(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 30),
-                new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 30),
-                new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 30),
-                new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 30));
-        return new Category("전자제품", electronicList);
-    }
-
-    public static Category createClothingCategory() {
-        List<Product> clothingList = List.of(new Product("의류1", 1200000, "최신 안드로이드 스마트폰", 30),
-                new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 30),
-                new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 30),
-                new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 30));
-        return new Category("의류", clothingList);
-    }
-
-    public static Category createFoodCategory() {
-        List<Product> foodList = List.of(new Product("식품1", 1200000, "최신 안드로이드 스마트폰", 30),
-                new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 30),
-                new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 30),
-                new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 30));
-        return new Category("식품", foodList);
-    }
-
     public String getCategoryName() {
         return categoryName;
     }
 
-    public void printCategory() {
-        Scanner sc = new Scanner(System.in);
+    // 등록된 상품의 수 가져오기
+    public int getProductSize() {
+        return products.size();
+    }
 
+    // 등록된 상품 중 하나 출력하기
+    public void printProducts(int i) {
+        products.get(i).printProduct();
+    }
+
+    // 카테고리 출력
+    public void printCategory() {
         System.out.println("[ " + categoryName + " 카테고리]");
         // 금액 콤마 포맷팅
         DecimalFormat df = new DecimalFormat("#,###");
@@ -56,17 +39,5 @@ public class Category {
                     i + 1, p.getProductName(), df.format(p.getPrice()), p.getProductDescription());
         }
         System.out.println("0. 뒤로가기");
-
-        int categoryOption = sc.nextInt();
-        for (int i = 0; i < products.size(); i++) {
-            if (categoryOption == 0){
-                break;
-            }
-            else if (categoryOption == i + 1) {
-                products.get(i).printProduct();
-            }
-        }
-
     }
-
 }
--- src/step3/CommerceSystem.java
+++ src/step3/CommerceSystem.java
@@ -1,5 +1,7 @@
 package step3;
 
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,14 +9,43 @@ public class CommerceSystem {
     private Scanner sc;
     private List<Category> category;
 
-    public CommerceSystem(Scanner sc) {
+    public CommerceSystem(Scanner sc, List<Category> category) {
         this.sc = sc;
-        this.category = List.of(Category.createElectronicsCategory(),
-                Category.createClothingCategory(),
-                Category.createFoodCategory()
-        );
+        this.category = category;
     }
 
+    public void getProductOption(int categoryNum) {
+        while (true) {
+            try {
+                int categoryOption = sc.nextInt();
+
+                // 1. 뒤로가기 (0번)
+                if (categoryOption == 0) {
+                    break;
+                }
+
+                // 선택된 카테고리 내부 상품의 개수
+                int size = category.get(categoryNum).getProductSize();
+
+                // 2. 정상 범위의 상품 선택 (1 ~ 상품 개수)
+                if (categoryOption > 0 && categoryOption <= size) {
+                    category.get(categoryNum).printProducts(categoryOption - 1);
+                    break;
+                    // 상품 상세 정보를 본 후 다시 목록을 보여줄지,
+                    // 아니면 여기서 break해서 뒤로 갈지 결정
+                } else {
+                    // 3. 범위를 벗어난 숫자 입력
+                    System.out.println("잘못된 번호입니다. 1번부터 "
+                            + (size + 1) + "번 사이의 숫자를 입력해주세요.");
+                }
+
+            } catch (InputMismatchException e) {
+                // 4. 문자가 입력되었을 때 예외 처리
+                System.out.println("숫자로만 입력해주세요");
+                sc.nextLine(); // 중요! 잘못 입력된 문자열을 비워줘야 무한 루프에 빠지지 않습니다.
+            }
+        }
+    }
 
     public void start() {
         while(true){
@@ -31,15 +62,23 @@ public class CommerceSystem {
             int mainOption = sc.nextInt();
 
             switch(mainOption) {
-                case 1 -> category.get(0).printCategory();
-                case 2 -> category.get(1).printCategory();
-                case 3 -> category.get(2).printCategory();
+                case 1 -> {
+                    category.get(0).printCategory();
+                    getProductOption(0);
+                }
+                case 2 -> {
+                    category.get(1).printCategory();
+                    getProductOption(1);
+                }
+                case 3 -> {
+                    category.get(2).printCategory();
+                    getProductOption(2);
+                }
                 case 0 -> System.out.println("커머스 플랫폼을 종료합니다.");
             }
             if (mainOption == 0) {
                 break;
             }
         }
-
     }
 }
